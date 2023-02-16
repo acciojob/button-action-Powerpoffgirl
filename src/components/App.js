efc19504b2ab22b768680b421ac21ee5cd06e513
user.Powerpoffgirl <email.jyotisingh13@gmail.com>
2023-02-16 18:58:34 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
+import paragraph from "./paragraph.js";
+import "./App.css";
+import Paragraph from "./Paragraph";
 
 function App() {
+  const [isHidden, setIsHidden] = useState(false);
+  function showText() {
+    setIsHidden(true);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <h1>Hello</h1>
+      {isHidden === true ? (
+        // <p id="para">
+        //   Hello, I've learnt to use the full-stack evaluation tool. This makes
+        //   me so happy
+        // </p>
+        <Paragraph />
+      ) : (
+        <p></p>
+      )}
+
+      <button id="click" onClick={showText}>
+        Click
+      </button>
     </div>
   );
 }
-
 
 export default App;
---
+++ src/components/Paragraph.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Paragraph.css";
+
+function Paragraph() {
+  return (
+    <div className="paragraph">
+      <p>
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy"
+      </p>
+    </div>
+  );
+}
+
+export default Paragraph;
