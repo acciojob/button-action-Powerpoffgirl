dde86c2073f228bbe119331cef167d850d856cb0
user.Powerpoffgirl <email.jyotisingh13@gmail.com>
2023-02-21 18:29:57 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component, useState } from "react";
 import "./../styles/App.css";
-import paragraph from "./paragraph.js";
+// import paragraph from "./paragraph.js";
 import "./App.css";
-import Paragraph from "./Paragraph";
+// import Paragraph from "./Paragraph";
 
 function App() {
   const [isHidden, setIsHidden] = useState(false);
@@ -13,11 +13,11 @@ function App() {
     <div id="main">
       <h1>Hello</h1>
       {isHidden === true ? (
-        // <p id="para">
-        //   Hello, I've learnt to use the full-stack evaluation tool. This makes
-        //   me so happy
-        // </p>
-        <Paragraph />
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+        // <Paragraph />
       ) : (
         <p></p>
       )}
